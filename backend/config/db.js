2e5167965a52dfad4ce1b3e2e1dbaf0b17631205
121140084_Rizki Esa Fadillah <126938786+121140084-Rizki-Esa-Fadillah@users.noreply.github.com>
2025-02-20 10:24:27 +0700

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connection = mongoose.createConnection('mongodb://localhost:27017/').on('open', ()=>{
-      console.log('Connected to MongoDB');
-}).on('error', ()=>{
-      console.log('Error connecting to MongoDB');
-});
-
-module.exports = connection;
