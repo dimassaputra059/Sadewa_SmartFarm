f3ed11b1b647ec8fa16179e337884827bc433b04
RizkiEsaFadillah <rizkiifadillah456@gmail.com>
2025-02-21 19:29:11 +0700

    rename file screen

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend_app/Screen/pages/splash_screen.dart';
+import 'package:frontend_app/presentation/pages/splash_screen.dart';
 
 void main() {
   runApp(const MyApp());
--- frontend/lib/presentation/pages/splash_screen.dart
+++ frontend/lib/presentation/pages/splash_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:frontend_app/Screen/widget/background_widget.dart';
+import 'package:frontend_app/presentation/widget/background_widget.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class SplashScreen extends StatelessWidget {
